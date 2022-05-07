3df7c4806eb54905230b83528168b6e46530a3da
Martin Kluska <martin@kluska.cz>
2022-05-07 17:14:18 +0200

    Lint fixes

--- src/saveToJSON.ts
+++ src/saveToJSON.ts
@@ -1,6 +1,7 @@
 import fs from "fs";
 import path from "path";
 
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
 export function saveJSON (content: any, file: string, outDir: string) {
     fs.writeFileSync(path.resolve(outDir, file), JSON.stringify(content, null, '\t'), 'utf8')
 }
