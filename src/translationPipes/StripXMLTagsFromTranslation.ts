c2574fe227b16d241c2cae21b83cff8d107bed0f
Juraj Uhlar <juraj.uhlar@flowmon.com>
2022-11-30 18:58:22 +0000

    Add explaining comment to strip SSML pipeline

--- src/translationPipes/StripXMLTagsFromTranslation.ts
+++ src/translationPipes/StripXMLTagsFromTranslation.ts
@@ -1,6 +1,13 @@
 import { Translation, TranslationPipe } from '../definitions.js'
 import { Language } from '../locales.js'
 
+/**
+ * Translations in Airtable can contain SSML tags that tell the Text-to-Speech engine
+ * how exactly to pronounce the phrase.
+ * https://learn.microsoft.com/en-us/azure/cognitive-services/speech-service/speech-synthesis-markup
+ * We need to strip them away when displaying the phrases on the web/apps.
+ * Example: `<sub alias="психіат‿р">психіатр</sub>` becomes just `психіатр`.
+ */
 export class StripXMLTagsFromTranslation implements TranslationPipe {
     async execute(
         _languagePack: Language,
